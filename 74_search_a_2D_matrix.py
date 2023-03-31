9b22fcdbd1122df395a17e2d0ded993a99a399eb
bgani <beisembayev.gk@gmail.com>
2023-03-31 12:23:26 +0600

    Add 74. Search a 2D Matrix

---
+++ 74_search_a_2D_matrix.py
@@ -0,0 +1,34 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        # top and bottom pointers
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1 # target might be in bottom subarrrays
+            elif target < matrix[row][0]:
+                bot = row - 1 # target might be in top subarrays
+            else: 
+                break # target might be in current row
+        
+        # if top and bot crossed each other, there is no target
+        if not (top <= bot):
+            return False
+        
+        # find current row by top and bot values from previous binary search on matrix rows
+        row = (top + bot) // 2
+
+        # now we do binary search on found row
+        # left and right pointers
+        l, r = 0, COLS 
+        while l <= r:
+            m = (l + r) // 2 # find mid
+            if target > matrix[row][m]:
+                l = m + 1 # eliminate left side, move left pointer to m + 1
+            elif target < matrix[row][m]:
+                r = m - 1 # eliminate right side, move right pointer to m - 1
+            else: 
+                return True # target is equal to mid
+        return False # left and right crossed each other, target not found
